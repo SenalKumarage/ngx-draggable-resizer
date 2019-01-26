df322a09dee04767f67c38d7ba2d55c0354fc604
Senal Kumarage <senalkumarage@gmail.com>
2019-01-27 01:03:01 +0800

    Completing properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-draggable-resizer';
+
+  onSizeChanged(ev) {
+    console.log(ev);
+  }
+
+  onPositionChange(ev) {
+    console.log(ev);
+  }
 }
--- src/packages/ngx-draggable-resizer/src/ngx-draggable-resizer.directive.ts
+++ src/packages/ngx-draggable-resizer/src/ngx-draggable-resizer.directive.ts
@@ -1,10 +1,10 @@
 import {
-    Directive,
-    ElementRef,
-    Output,
-    EventEmitter,
-    OnInit,
-    Input
+  Directive,
+  ElementRef,
+  Output,
+  EventEmitter,
+  OnInit,
+  Input
 } from '@angular/core';
 import * as jquery from 'jquery';
 import 'jquery-ui/ui/widgets/draggable';
@@ -13,75 +13,57 @@ import 'jquery-ui/ui/widgets/resizable';
 const $ = jquery;
 
 @Directive({
-    // tslint:disable-next-line:directive-selector
-    selector: '[draggableResizer]'
+  // tslint:disable-next-line:directive-selector
+  selector: '[draggableResizer]'
 })
 export class DraggableResizerDirective implements OnInit {
 
-    @Input() public draggableOnly: boolean;
+  @Input() public draggableOnly: boolean;
 
-    @Input() public resizableOnly: boolean;
+  @Input() public resizableOnly: boolean;
 
-    @Output() public sizeChanged: EventEmitter<Size> = new EventEmitter();
-    @Output() public positionChange: EventEmitter<Position> = new EventEmitter();
+  @Output() public sizeChanged: EventEmitter<Size> = new EventEmitter();
+  @Output() public positionChange: EventEmitter<Position> = new EventEmitter();
 
-    constructor(private _elemRef: ElementRef) { }
+  constructor(private _elemRef: ElementRef) { }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.loadIfNot('./draggable.css', 'draggable-stylesheet');
-        this.loadIfNot('./resizable.css', 'resizable-stylesheet');
+    const thisElement = <any>$(this._elemRef.nativeElement);
+    const parent = $('.field-container');
 
-        const thisElement = <any>$(this._elemRef.nativeElement);
-        const parent = $('.field-container');
+    if (!this.draggableOnly) {
 
-        if (!this.draggableOnly) {
-
-            thisElement.resizable({
-                stop: (event, ui) => {
-                    this.sizeChanged.emit(ui.size);
-                }
-            });
+      thisElement.resizable({
+        stop: (event, ui) => {
+          this.sizeChanged.emit(ui.size);
         }
-
-
-        if (!this.resizableOnly) {
-
-            thisElement.draggable({
-                containment: '.field-container',
-                stop: (event, ui) => {
-                    this.positionChange.emit({
-                        top: $(this._elemRef.nativeElement).css('top'),
-                        left: $(this._elemRef.nativeElement).css('left')
-                    });
-                }
-            });
-        }
-
+      });
     }
 
-    loadIfNot(url, cssId) {
 
-        if (!document.getElementById(cssId)) {
-            const head = document.getElementsByTagName('head')[0];
-            const link = document.createElement('link');
-            link.id = cssId;
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            link.href = url;
-            link.media = 'all';
-            head.appendChild(link);
+    if (!this.resizableOnly) {
+
+      thisElement.draggable({
+        containment: '.field-container',
+        stop: (event, ui) => {
+          this.positionChange.emit({
+            top: $(this._elemRef.nativeElement).css('top'),
+            left: $(this._elemRef.nativeElement).css('left')
+          });
         }
-
+      });
     }
+
+  }
 }
 
 export interface Position {
-    top: string;
-    left: string;
+  top: string;
+  left: string;
 }
 
 export interface Size {
-    height: number;
-    width: number;
+  height: number;
+  width: number;
 }
